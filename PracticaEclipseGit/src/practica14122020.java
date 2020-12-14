1f27f113f520f8cc911f13a99a46ca9616ffa8c2
raulmarinol <alum.rmarinol.2020@iesalixar.org>
2020-12-14 12:45:33 +0100

    hola

---
+++ PracticaEclipseGit/src/practica14122020.java
@@ -0,0 +1,15 @@
+
+public class practica14122020 {
+	public class HelloWorld
+	{
+	    // Tu programa comienza con una llamada a main().
+	    // Imprime "Hola Mundo" a la ventana de la terminal.
+	    public static void main(String args[])
+	    {
+	        System.out.println("Hola Mundo");
+	        System.out.println("Como estas");
+	    }
+	}
+
+
+}
